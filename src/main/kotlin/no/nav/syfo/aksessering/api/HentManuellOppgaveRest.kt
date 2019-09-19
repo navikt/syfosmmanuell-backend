9ab25b5200d4c08767c586a8f48605cddef5d807
Joakim Kartveit <joakimkartveit@gmail.com>
2019-09-19 09:16:30 +0200

    Legger til logging info

--- src/main/kotlin/no/nav/syfo/aksessering/api/HentManuellOppgaveRest.kt
+++ src/main/kotlin/no/nav/syfo/aksessering/api/HentManuellOppgaveRest.kt
@@ -7,11 +7,13 @@ import io.ktor.routing.Routing
 import io.ktor.routing.get
 import io.ktor.routing.route
 import no.nav.syfo.aksessering.ManuellOppgaveService
+import no.nav.syfo.log
 
 fun Routing.hentManuellOppgave(manuellOppgaveService: ManuellOppgaveService) {
     route("/api/v1") {
-        get("/manuellOppgave") {
-            val manuellOppgaveId = call.parameters["manuellopgaveId"]
+        get("/manuelloppgave") {
+            log.info("Recived call to /api/v1/manuelloppgave")
+            val manuellOppgaveId = call.request.queryParameters["manuelloppgaveId"]
 
             when (manuellOppgaveId) {
                 null -> call.respond(HttpStatusCode.BadRequest)
