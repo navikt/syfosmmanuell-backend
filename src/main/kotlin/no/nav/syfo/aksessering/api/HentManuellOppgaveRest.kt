653ba1cd64e2052955e62646cb052a0cbb56f80f
Joakim Kartveit <joakimkartveit@gmail.com>
2019-09-25 13:52:07 +0200

    Oppdatere med eit nytt get endepunkt, som skal bare svare om personen har noen manuell oppgaver som ikkje er ferdigstilt enda. Og endrer navn på begge get endepunktene

--- src/main/kotlin/no/nav/syfo/aksessering/api/HentManuellOppgaveRest.kt
+++ src/main/kotlin/no/nav/syfo/aksessering/api/HentManuellOppgaveRest.kt
@@ -12,8 +12,8 @@ import no.nav.syfo.service.ManuellOppgaveService
 
 fun Routing.hentManuellOppgaver(manuellOppgaveService: ManuellOppgaveService) {
     route("/api/v1") {
-        get("/manuelloppgave") {
-            log.info("Recived call to /api/v1/manuelloppgave")
+        get("/hentManuellOppgave") {
+            log.info("Recived call to /api/v1/hentManuellOppgave")
             val pasientFnr = call.request.queryParameters["fnr"]
 
             if (pasientFnr.isNullOrEmpty()) {
@@ -24,6 +24,22 @@ fun Routing.hentManuellOppgaver(manuellOppgaveService: ManuellOppgaveService) {
                 call.respond(emptyList<ManuellOppgaveDTO>())
             } else {
                 call.respond(manuellOppgaveService.hentManuellOppgaver(pasientFnr))
+            }
+        }
+
+        get("/harManuellOppgave") {
+            log.info("Recived call to /api/v1/harManuellOppgave")
+            val pasientFnr = call.request.queryParameters["fnr"]
+
+            if (pasientFnr.isNullOrEmpty()) {
+                log.info("Mangler query parameters: fnr")
+                call.respond(HttpStatusCode.BadRequest)
+            } else if (manuellOppgaveService.hentManuellOppgaver(pasientFnr).isEmpty()) {
+                log.info("Fant ingen ikke ferdigstile manuelloppgaver for akutell fnr")
+                call.respond(false)
+            } else {
+                log.info("Fant ikke ferdigstile manuelloppgaver for akutell fnr")
+                call.respond(true)
             }
         }
     }
