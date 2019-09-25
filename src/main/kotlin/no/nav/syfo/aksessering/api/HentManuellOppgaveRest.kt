f45d60e0a5e651b69ca53c1989c91709fc096bf8
Joakim Kartveit <joakimkartveit@gmail.com>
2019-09-25 13:29:23 +0200

    Sender tilbake ein tom liste dersom, det ikkje finnes noen manuell oppgaver på aktuellt fnr

--- src/main/kotlin/no/nav/syfo/aksessering/api/HentManuellOppgaveRest.kt
+++ src/main/kotlin/no/nav/syfo/aksessering/api/HentManuellOppgaveRest.kt
@@ -6,6 +6,7 @@ import io.ktor.response.respond
 import io.ktor.routing.Routing
 import io.ktor.routing.get
 import io.ktor.routing.route
+import no.nav.syfo.aksessering.ManuellOppgaveDTO
 import no.nav.syfo.log
 import no.nav.syfo.service.ManuellOppgaveService
 
@@ -19,8 +20,8 @@ fun Routing.hentManuellOppgaver(manuellOppgaveService: ManuellOppgaveService) {
                 log.info("Mangler query parameters: fnr")
                 call.respond(HttpStatusCode.BadRequest)
             } else if (manuellOppgaveService.hentManuellOppgaver(pasientFnr).isEmpty()) {
-                log.info("Fant ingen manuelloppgaver for akutell fnr")
-                call.respond(HttpStatusCode.BadRequest)
+                log.info("Fant ingen ikke ferdigstile manuelloppgaver for akutell fnr")
+                call.respond(emptyList<ManuellOppgaveDTO>())
             } else {
                 call.respond(manuellOppgaveService.hentManuellOppgaver(pasientFnr))
             }
