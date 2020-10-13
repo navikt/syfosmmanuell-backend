9c050e78c0a36f6bfe3ada3b27994ffcee0395a5
Tia Firing <tia.firing@nav.no>
2020-10-13 11:47:56 +0200

    oppdatert tekst

--- src/main/kotlin/no/nav/syfo/persistering/api/SendVurderingManuellOppgaveRest.kt
+++ src/main/kotlin/no/nav/syfo/persistering/api/SendVurderingManuellOppgaveRest.kt
@@ -63,12 +63,12 @@ fun Result.tilValidationResult(): ValidationResult {
 enum class RuleInfoTekst(val messageForUser: String, val messageForSender: String, val rulename: String) {
     TILBAKEDATERT_MANGLER_BEGRUNNELSE(
         "Sykmelding gjelder som hovedregel fra den dagen du oppsøker behandler. Sykmeldingen din er tilbakedatert uten at det er gitt en god nok begrunnelse for dette. Behandleren din må skrive ut en ny sykmelding og begrunne bedre hvorfor den er tilbakedatert. Din behandler har mottatt melding fra NAV om dette.",
-        "Sykmelding gjelder som hovedregel fra den dagen pasienten oppsøker behandler. Sykmeldingen er tilbakedatert uten at det kommer tydelig nok fram hvorfor dette var nødvendig. Sykmeldingen er derfor avvist, og det må skrives en ny hvis sykmelding fortsatt er aktuelt. Pasienten har fått beskjed.",
+        "Sykmelding gjelder som hovedregel fra den dagen pasienten oppsøker behandler. Sykmeldingen er tilbakedatert uten at det kommer tydelig nok fram hvorfor dette var nødvendig. Sykmeldingen er derfor avvist, og det må skrives en ny hvis det fortsatt er aktuelt med sykmelding. Pasienten har fått beskjed om å vente på ny sykmelding fra deg.",
         "TILBAKEDATERT_MANGLER_BEGRUNNELSE"
     ),
     TILBAKEDATERT_IKKE_GODTATT(
         "NAV kan ikke godta sykmeldingen din fordi den starter før dagen du tok kontakt med behandleren. Trenger du fortsatt sykmelding, må behandleren din skrive en ny som gjelder fra den dagen dere var i kontakt. Behandleren din har fått beskjed fra NAV om dette.",
-        "NAV kan ikke godta tilbakedateringen. Sykmeldingen er derfor avvist. Hvis sykmelding fortsatt er aktuelt, må det skrives ny sykmelding der f.o.m.-dato er dagen du var i kontakt med pasienten. Pasienten har fått beskjed.",
+        "NAV kan ikke godta tilbakedateringen. Sykmeldingen er derfor avvist. Hvis det fortsatt er aktuelt med sykmelding, må det skrives ny sykmelding der f.o.m.-dato er dagen du var i kontakt med pasienten. Pasienten har fått beskjed om å vente på ny sykmelding fra deg.",
         "TILBAKEDATERT_IKKE_GODTATT"
     )
 }
