be76fc8b68be4e292e70a20c13b345d72f7f7294
Sebastian Knudsen <sebastian.knudsen@nav.no>
2021-02-04 09:21:36 +0100

    Implement design changes

--- src/main/kotlin/no/nav/syfo/persistering/api/SendVurderingManuellOppgaveRest.kt
+++ src/main/kotlin/no/nav/syfo/persistering/api/SendVurderingManuellOppgaveRest.kt
@@ -42,7 +42,7 @@ fun Route.sendVurderingManuellOppgave(
                     call.respond(HttpStatusCode.NotFound)
                 }
                 true -> {
-                    val result: Result = call.receive()
+                    val result = call.receive<Result>()
 
                     val validationResult = result.tilValidationResult()
 
@@ -65,49 +65,103 @@ fun Route.sendVurderingManuellOppgave(
     }
 }
 
-fun Result.tilValidationResult(): ValidationResult {
-    return when (status) {
-        ResultStatus.GODKJENT -> ValidationResult(Status.OK, emptyList())
-        ResultStatus.UGYLDIG_TILBAKEDATERING -> ValidationResult(Status.OK, emptyList())
-        ResultStatus.UGYLDIG_BEGRUNNELSE -> {
-            val regel = RuleInfoTekst.TILBAKEDATERT_MANGLER_BEGRUNNELSE
-            ValidationResult(
-                Status.INVALID,
-                listOf(
-                    RuleInfo(regel.name, regel.messageForSender, regel.messageForUser, Status.INVALID)
-                )
-            )
-        }
-    }
-}
-
-fun Result.tilMerknad(): Merknad? {
-    return when (status) {
-        ResultStatus.UGYLDIG_TILBAKEDATERING -> {
-            Merknad(type = MerknadType.UGYLDIG_TILBAKEDATERING.name, beskrivelse = "Tilbakedateringen er vurdert som ugyldig, men brukeren får allikevel sende søknad slik at et vedtak kan fattes")
-        }
-        else -> null
-    }
-}
-
+// TODO: remove when refactoring tests
 enum class RuleInfoTekst(val messageForUser: String, val messageForSender: String, val rulename: String) {
-    TILBAKEDATERT_MANGLER_BEGRUNNELSE(
+    MANGLER_BEGRUNNELSE(
         messageForUser = "Sykmeldingen din starter før du oppsøkte behandleren, uten at det er gitt en god nok begrunnelse for dette.",
         messageForSender = "Sykmelding gjelder som hovedregel fra den dagen pasienten oppsøker behandler. Sykmeldingen er tilbakedatert uten at det kommer tydelig nok fram hvorfor dette var nødvendig. Sykmeldingen er derfor avvist, og det må skrives en ny hvis det fortsatt er aktuelt med sykmelding. Pasienten har fått beskjed om å vente på ny sykmelding fra deg.",
         rulename = "TILBAKEDATERT_MANGLER_BEGRUNNELSE"
+    ),
+    UGYLDIG_TILBAKEDATERING(
+        messageForUser = "Sykmeldingen din starter før du oppsøkte behandleren, uten at det er gitt en god nok begrunnelse for dette.",
+        messageForSender = "Sykmelding gjelder som hovedregel fra den dagen pasienten oppsøker behandler. Sykmeldingen er tilbakedatert uten at det kommer tydelig nok fram hvorfor dette var nødvendig. Sykmeldingen er derfor avvist, og det må skrives en ny hvis det fortsatt er aktuelt med sykmelding. Pasienten har fått beskjed om å vente på ny sykmelding fra deg.",
+        rulename = "UGYLDIG_TILBAKEDATERING"
     )
 }
 
 enum class MerknadType {
-    UGYLDIG_TILBAKEDATERING
+    UGYLDIG_TILBAKEDATERING,
+    KREVER_FLERE_OPPLYSNINGER
+}
+
+enum class AvvisningType {
+    MANGLER_BEGRUNNELSE,
+    UGYLDIG_BEGRUNNELSE
 }
 
 enum class ResultStatus {
     GODKJENT,
-    UGYLDIG_TILBAKEDATERING,
-    UGYLDIG_BEGRUNNELSE
+    GODKJENT_MED_MERKNAD,
+    AVVIST
 }
 
 data class Result(
-    val status: ResultStatus
-)
+    val status: ResultStatus,
+    val merknad: MerknadType?,
+    val avvisningtype: AvvisningType?,
+) {
+    fun toMerknad(): Merknad? {
+        return when (status) {
+            ResultStatus.GODKJENT_MED_MERKNAD -> {
+                return when (merknad) {
+                    MerknadType.UGYLDIG_TILBAKEDATERING -> {
+                        Merknad(
+                            type=MerknadType.UGYLDIG_TILBAKEDATERING.name,
+                            beskrivelse="Tilbakedateringen er vurdert som ugyldig, men brukeren får allikevel sende søknad slik at et vedtak kan fattes"
+                        )
+                    }
+                    MerknadType.KREVER_FLERE_OPPLYSNINGER -> {
+                        Merknad(
+                            type=MerknadType.KREVER_FLERE_OPPLYSNINGER.name,
+                            beskrivelse=""
+                        )
+                    }
+                    else -> {
+                        throw RuntimeException("Result with status GODKJENT_MED_MERKNAD missing merknad property")
+                    }
+                }
+            },
+            else -> null
+        }
+    }
+
+    fun toValidationResult(): ValidationResult {
+        return when (status) {
+            ResultStatus.GODKJENT -> ValidationResult(Status.OK, emptyList())
+            ResultStatus.GODKJENT_MED_MERKNAD -> ValidationResult(Status.OK, emptyList())
+            ResultStatus.AVVIST -> {
+                return when (avvisningtype) {
+                    AvvisningType.MANGLER_BEGRUNNELSE -> {
+                        ValidationResult(
+                            Status.INVALID,
+                            listOf(
+                                RuleInfo(
+                                    ruleName="TILBAKEDATERT_MANGLER_BEGRUNNELSE",
+                                    messageForSender="",
+                                    messageForUser="",
+                                    Status.INVALID
+                                )
+                            )
+                        )
+                    },
+                    AvvisningType.UGYLDIG_BEGRUNNELSE -> {
+                        ValidationResult(
+                            Status.INVALID,
+                            listOf(
+                                RuleInfo(
+                                    ruleName="TILBAKEDATERT_MANGLER_BEGRUNNELSE",
+                                    messageForSender="",
+                                    messageForUser="",
+                                    Status.INVALID
+                                )
+                            )
+                        )
+                    }
+                    else -> {
+                        throw RuntimeException("Result with status AVVIST missing avvisningtype property")
+                    }
+                }
+            }
+        }
+    }
+}
