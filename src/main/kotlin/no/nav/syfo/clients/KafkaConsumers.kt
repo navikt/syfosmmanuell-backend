0205b2325e92059fa5eacf62250f7a147052fe53
Tia Firing <tia.firing@nav.no>
2022-01-17 08:58:10 +0100

    setter opp dobbel consumer for manuell-topic

--- src/main/kotlin/no/nav/syfo/clients/KafkaConsumers.kt
+++ src/main/kotlin/no/nav/syfo/clients/KafkaConsumers.kt
@@ -2,8 +2,10 @@ package no.nav.syfo.clients
 
 import no.nav.syfo.Environment
 import no.nav.syfo.kafka.KafkaCredentials
+import no.nav.syfo.kafka.aiven.KafkaUtils
 import no.nav.syfo.kafka.loadBaseConfig
 import no.nav.syfo.kafka.toConsumerConfig
+import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.common.serialization.StringDeserializer
 
@@ -18,5 +20,16 @@ class KafkaConsumers(env: Environment, vaultSecrets: KafkaCredentials) {
         "${env.applicationName}-consumer", valueDeserializer = StringDeserializer::class
     )
 
+    private val consumerPropertiesAiven = KafkaUtils.getAivenKafkaConfig().also {
+        it.let {
+            it[ConsumerConfig.MAX_POLL_RECORDS_CONFIG] = "1"
+            it[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = "earliest"
+        }
+    }.toConsumerConfig(
+        "${env.applicationName}-consumer", valueDeserializer = StringDeserializer::class
+    )
+
     val kafkaConsumerManuellOppgave = KafkaConsumer<String, String>(properties)
+
+    val kafkaAivenConsumerManuellOppgave = KafkaConsumer<String, String>(consumerPropertiesAiven)
 }
