eaf0d52d5ba86e4a7300bdcdaeeba4303691eb2f
Tia Firing <tia.firing@nav.no>
2022-05-16 10:58:25 +0200

    justerer timeout

--- src/main/kotlin/no/nav/syfo/application/ApplicationServer.kt
+++ src/main/kotlin/no/nav/syfo/application/ApplicationServer.kt
@@ -18,7 +18,7 @@ class ApplicationServer(
     }
 
     fun start() {
-        applicationServer.start(wait = false)
+        applicationServer.start(wait = true)
         applicationState.alive = true
     }
 }
