26fc3718a7d3ea929412be384ac2b211fb4894d3
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2022-08-03 16:00:08 +0200

    Upgraded several dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,11 +5,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 group = "no.nav.syfo"
 version = "1.0.0"
 
-val coroutinesVersion = "1.6.1"
-val ktorVersion = "2.0.1"
+val coroutinesVersion = "1.6.4"
+val ktorVersion = "2.0.3"
 val logbackVersion = "1.2.11"
-val logstashEncoderVersion = "7.1.1"
-val prometheusVersion = "0.15.0"
+val logstashEncoderVersion = "7.2"
+val prometheusVersion = "0.16.0"
 val smCommonVersion = "1.f132f2b"
 val sykmeldingVersion = "2019.07.29-02-53-86b22e73f7843e422ee500b486dac387a582f2d1"
 val fellesformatVersion = "2019.07.30-12-26-5c924ef4f04022bbb850aaf299eb8e4464c1ca6a"
@@ -17,20 +17,20 @@ val kithHodemeldingVersion = "2019.07.30-12-26-5c924ef4f04022bbb850aaf299eb8e446
 val jacksonVersion = "2.13.3"
 val kluentVersion = "1.68"
 val mockkVersion = "1.12.4"
-val postgresVersion = "42.3.4"
-val flywayVersion = "8.5.10"
+val postgresVersion = "42.3.6"
+val flywayVersion = "8.5.12"
 val hikariVersion = "5.0.1"
 val vaultJavaDriveVersion = "3.1.0"
 val javaTimeAdapterVersion = "1.1.3"
-val kotestVersion = "5.3.0"
+val kotestVersion = "5.4.0"
 val nimbusdsVersion = "9.22"
-val caffeineVersion = "3.1.0"
-val testContainerVersion = "1.17.1"
-val kotlinVersion = "1.6.21"
+val caffeineVersion = "3.1.1"
+val testContainerVersion = "1.17.3"
+val kotlinVersion = "1.7.10"
 val kafkaVersion = "3.1.0"
 
 plugins {
-    kotlin("jvm") version "1.6.21"
+    kotlin("jvm") version "1.7.10"
     id("com.github.johnrengelman.shadow") version "7.1.2"
     id("org.jmailen.kotlinter") version "3.10.0"
     id("com.diffplug.spotless") version "6.5.0"
@@ -135,7 +135,11 @@ tasks {
         dependsOn("lintKotlin")
         useJUnitPlatform {
         }
-        testLogging.showStandardStreams = true
+        testLogging {
+            events("skipped", "failed")
+            showStackTraces = true
+            exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+        }
     }
 
     "check" {
