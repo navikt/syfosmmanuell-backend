312ac060b1c02f8ab88d0bdeb36136a71837f98b
Andreas Nilsen <danduras@gmail.com>
2022-11-29 14:52:44 +0100

    use default settings

--- src/main/kotlin/no/nav/syfo/db/Database.kt
+++ src/main/kotlin/no/nav/syfo/db/Database.kt
@@ -28,10 +28,8 @@ class Database(private val env: Environment, retries: Long = 30, sleepTime: Long
                         jdbcUrl = "jdbc:postgresql://${env.dbHost}:${env.dbPort}/${env.dbName}"
                         username = env.databaseUsername
                         password = env.databasePassword
-                        maximumPoolSize = 10
+                        maximumPoolSize = 5
                         minimumIdle = 3
-                        idleTimeout = 10000
-                        maxLifetime = 300000
                         isAutoCommit = false
                         transactionIsolation = "TRANSACTION_REPEATABLE_READ"
                         validate()
