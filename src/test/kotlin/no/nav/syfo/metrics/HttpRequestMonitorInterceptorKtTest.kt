bae6d208ba27f00869f7f47fa8eca8215a2de95b
tiafiring <tia.firing@nav.no>
2022-12-21 12:23:16 +0100

    maskerer sykmeldingid fra metrikker (#73)

--- src/test/kotlin/no/nav/syfo/metrics/HttpRequestMonitorInterceptorKtTest.kt
+++ src/test/kotlin/no/nav/syfo/metrics/HttpRequestMonitorInterceptorKtTest.kt
@@ -2,15 +2,24 @@ package no.nav.syfo.metrics
 
 import io.kotest.core.spec.style.FunSpec
 import org.junit.jupiter.api.Assertions.assertEquals
+import java.util.UUID
 
 class HttpRequestMonitorInterceptorKtTest : FunSpec({
     context("Regex for metrikker") {
-        test("Skal matche på aktørid") {
+        test("Skal matche på oppgaveId") {
             val path = "/manuellOppgave/334866611"
 
-            val oppdatertPath = REGEX.replace(path, ":oppgaveId")
+            val oppdatertPath = getLabel(path)
 
             assertEquals("/manuellOppgave/:oppgaveId", oppdatertPath)
+        }
+        test("Skal matche på sykmeldingId") {
+            val sykmeldingId = UUID.randomUUID().toString()
+            val path = "/sykmelding/$sykmeldingId"
+
+            val oppdatertPath = getLabel(path)
+
+            assertEquals("/sykmelding/:sykmeldingId", oppdatertPath)
         }
     }
 })
