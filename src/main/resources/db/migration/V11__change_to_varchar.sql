569aa74ddac6aa11d8cff6a0659d13b5e22def0c
Andreas <danduras@gmail.com>
2023-07-17 17:40:04 +0200

    Update V11__change_to_varchar.sql

--- src/main/resources/db/migration/V11__change_to_varchar.sql
+++ src/main/resources/db/migration/V11__change_to_varchar.sql
@@ -1,2 +1,1 @@
-ALTER TABLE manuelloppgave ALTER COLUMN id TYPE varchar USING trim(id);
-ALTER TABLE manuelloppgave ALTER COLUMN pasientfnr TYPE varchar USING trim(pasientfnr);
+ALTER TABLE manuelloppgave ALTER COLUMN id TYPE varchar;
