029f6b0eb709d49b2c19a0903b94a48d71b4c3bc
Andreas Sagen Aspaas <danduras@gmail.com>
2026-01-19 15:01:28 +0100

    chore: remove logging of claims

--- src/main/kotlin/no/nav/syfo/util/Utils.kt
+++ src/main/kotlin/no/nav/syfo/util/Utils.kt
@@ -1,19 +1,17 @@
 package no.nav.syfo.util
 
 import com.auth0.jwt.JWT
-import io.ktor.http.auth.HttpAuthHeader
-import io.ktor.server.auth.parseAuthorizationHeader
-import io.ktor.server.request.ApplicationRequest
-import no.nav.syfo.objectMapper
+import io.ktor.http.auth.*
+import io.ktor.server.auth.*
+import io.ktor.server.request.*
+import no.nav.syfo.sikkerlogg
 import java.io.IOException
 import java.net.URISyntaxException
-import no.nav.syfo.sikkerlogg
 
 @Throws(IOException::class, URISyntaxException::class)
 fun getAccessTokenFromAuthHeader(request: ApplicationRequest): String {
     val authHeader = request.parseAuthorizationHeader() ?: throw UnauthorizedException()
     val accessToken = (authHeader as HttpAuthHeader.Single).blob
-    logJwtClaims(accessToken)
     return accessToken
 }
 
@@ -28,10 +26,4 @@ fun logNAVEpostFromTokenWhenNoAccessToSecureLogs(token: String, path: String) {
     } catch (exception: Exception) {
         sikkerlogg.info("Fikk ikkje hentet ut navEpost", exception)
     }
-}
-
-
-fun logJwtClaims(token: String) {
-    val claims = JWT.decode(token).claims.mapValues { it.value.asString() }
-    sikkerlogg.info("Claims from jwt ${objectMapper.writeValueAsString(claims)}")
 }
