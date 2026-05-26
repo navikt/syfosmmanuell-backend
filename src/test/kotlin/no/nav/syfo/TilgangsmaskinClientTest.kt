67191a127cbc699bce28793e1b4fb1b23255fa7a
Helene Arnesen <helene.arnesen@nav.no>
2026-05-26 13:47:15 +0200

    fix: and rename tilgangsmaskinTest

--- src/test/kotlin/no/nav/syfo/TilgangsmaskinClientTest.kt
+++ src/test/kotlin/no/nav/syfo/TilgangsmaskinClientTest.kt
@@ -7,10 +7,8 @@ import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
 import io.mockk.spyk
-import no.nav.syfo.azuread.v2.AzureAdV2Client
-import no.nav.syfo.azuread.v2.AzureAdV2TokenResponse
 import no.nav.syfo.client.TexasClient
-import no.nav.syfo.client.Tilgang
+import no.nav.syfo.client.TexasToken
 import no.nav.syfo.client.TilgangsmaskinClient
 import no.nav.syfo.testutil.HttpClientTest
 import no.nav.syfo.testutil.ResponseData
@@ -20,14 +18,13 @@ class IstilgangskontrollClientTest :
     FunSpec({
         val httpClient = HttpClientTest()
         val environment = mockk<Environment>()
-        val azureAdV2Client =
-            spyk(AzureAdV2Client("foo", "bar", "http://obo", httpClient.httpClient))
-        val texasClient = TexasClient(httpClient.httpClient, environment)
+        val texasClient = mockk<TexasClient>()
 
         val pasientFnr = "123145"
 
         coEvery { environment.tilgangsmaskinUrl } returns "http://foo"
         coEvery { environment.tilgangsmaskinScope } returns "scope"
+        coEvery { texasClient.exchangeToken(any(), any()) } returns TexasToken("obo-token")
 
         val tilgangsmaskinClient =
             spyk(
@@ -39,39 +36,22 @@ class IstilgangskontrollClientTest :
             )
 
         beforeTest {
-            clearMocks(environment, azureAdV2Client)
+            clearMocks(texasClient)
+            coEvery { texasClient.exchangeToken(any(), any()) } returns TexasToken("obo-token")
             tilgangsmaskinClient.tilgangsmaskinCache.invalidateAll()
         }
 
         context("Tilgangskontroll-test") {
             test("Skal returnere erGodkjent = true") {
-                httpClient.responseDataOboToken =
-                    ResponseData(
-                        HttpStatusCode.OK,
-                        objectMapper.writeValueAsString(
-                            AzureAdV2TokenResponse("token", 1000000, "token_type")
-                        ),
-                    )
-                httpClient.responseData =
-                    ResponseData(HttpStatusCode.OK, objectMapper.writeValueAsString(Tilgang(true)))
+                httpClient.responseData = ResponseData(HttpStatusCode.NoContent, "")
 
                 val tilgang =
                     tilgangsmaskinClient.sjekkVeiledersTilgangTilPerson("sdfsdfsfs", pasientFnr)
 
                 assertEquals(true, tilgang.erGodkjent)
             }
-            test(
-                "Skal returnere erGodkjent = false hvis istilgangskontroll svarer med feilmelding"
-            ) {
-                httpClient.responseDataOboToken =
-                    ResponseData(
-                        HttpStatusCode.OK,
-                        objectMapper.writeValueAsString(
-                            AzureAdV2TokenResponse("token", 1000000, "token_type")
-                        )
-                    )
-                httpClient.responseData =
-                    ResponseData(HttpStatusCode.OK, objectMapper.writeValueAsString(Tilgang(false)))
+            test("Skal returnere erGodkjent = false hvis tilgangsmaskin svarer med 403") {
+                httpClient.responseData = ResponseData(HttpStatusCode.Forbidden, "")
 
                 val tilgang =
                     tilgangsmaskinClient.sjekkVeiledersTilgangTilPerson("sdfsdfsfs", pasientFnr)
@@ -82,23 +62,29 @@ class IstilgangskontrollClientTest :
 
         context("Test av cache") {
             test("Henter fra cache hvis kallet er cachet") {
+                httpClient.responseData = ResponseData(HttpStatusCode.NoContent, "")
+
                 tilgangsmaskinClient.sjekkVeiledersTilgangTilPerson("sdfsdfsfs", pasientFnr)
                 tilgangsmaskinClient.sjekkVeiledersTilgangTilPerson("sdfsdfsfs", pasientFnr)
 
-                coVerify(exactly = 1) { azureAdV2Client.getOnBehalfOfToken("sdfsdfsfs", "scope") }
+                coVerify(exactly = 1) { texasClient.exchangeToken("scope", "sdfsdfsfs") }
             }
             test("Henter ikke fra cache hvis samme accesstoken men ulikt fnr") {
+                httpClient.responseData = ResponseData(HttpStatusCode.NoContent, "")
+
                 tilgangsmaskinClient.sjekkVeiledersTilgangTilPerson("sdfsdfsfs", pasientFnr)
                 tilgangsmaskinClient.sjekkVeiledersTilgangTilPerson("sdfsdfsfs", "987654")
 
-                coVerify(exactly = 2) { azureAdV2Client.getOnBehalfOfToken("sdfsdfsfs", "scope") }
+                coVerify(exactly = 2) { texasClient.exchangeToken("scope", "sdfsdfsfs") }
             }
             test("Henter ikke fra cache hvis samme fnr men ulikt accesstoken") {
+                httpClient.responseData = ResponseData(HttpStatusCode.NoContent, "")
+
                 tilgangsmaskinClient.sjekkVeiledersTilgangTilPerson("sdfsdfsfs", pasientFnr)
                 tilgangsmaskinClient.sjekkVeiledersTilgangTilPerson("xxxxxxxxx", pasientFnr)
 
-                coVerify(exactly = 1) { azureAdV2Client.getOnBehalfOfToken("sdfsdfsfs", "scope") }
-                coVerify(exactly = 1) { azureAdV2Client.getOnBehalfOfToken("xxxxxxxxx", "scope") }
+                coVerify(exactly = 1) { texasClient.exchangeToken("scope", "sdfsdfsfs") }
+                coVerify(exactly = 1) { texasClient.exchangeToken("scope", "xxxxxxxxx") }
             }
         }
     })
